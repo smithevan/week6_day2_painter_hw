c1270b49e5d190beb3b3362994241260870bc617
Evan Smith <smith.m.evan@gmail.com>
2020-01-07 16:56:14 +0000

    decorator gets working

---
+++ decorator.js
@@ -0,0 +1,6 @@
+const Decorator = function (stock) {
+  this.stock = stock;
+}
+
+
+module.exports = Decorator; 
--- paint_specs/decorator_spec.js
+++ paint_specs/decorator_spec.js
@@ -1,3 +1,16 @@
-// const assert = require('assert');
-// const Decorator = require('../paint.js')
+const assert = require('assert');
+const Decorator = require('../decorator.js')
 // const Decorator = require('../room.js')
+
+describe('Decorator', function () {
+
+  beforeEach(function() {
+    decorator = new Decorator(8);
+  })
+
+  it('should check that decorator has stock', function() {
+    const actual = decorator.stock;
+    assert.strictEqual(actual, 8)
+  })
+
+})
